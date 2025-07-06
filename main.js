61820014295c658d0527b8fd08cfff6678949a95
Yogita2409 <150928540+Yogita2409@users.noreply.github.com>
2025-07-06 19:19:11 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new typed(".text",{
+    strings:["Frontend Development", "Web Developer", "Web Designing"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
