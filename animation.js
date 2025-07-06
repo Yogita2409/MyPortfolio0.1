85de26312a6466eccfce06e429661aa422d776f5
Yogita2409 <150928540+Yogita2409@users.noreply.github.com>
2025-07-07 01:06:59 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-  strings: ["Frontend Development", "Web Developer", "Web Designing"],
+  strings: ["Frontend Developer", "Web Developer", "Web Designing"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
